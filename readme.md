d0a386a1d2010afb60db0e5e8c7d050717b05579
thurendous <thurendous2@gmail.com>
2020-10-14 01:32:16 +0900

    daily grind

--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
+# Keep Practising Every Day
 This is just a Repository for learning about the Ethereum world. Welcome to the value of the internet or the internet of money.
+
+
+
+
+
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,13 +2,20 @@ const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
+//这里使用的provider是我们使用的来链接ganache的情况下使用的。
 // here we use Web3 only one time. Cuz we just want to 
+const { interface, bytecode } = require("../compile");
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
     // Get a list of all accounts
     accounts = await web3.eth.getAccounts();
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ["Hi there! "] })
+        .send({ from: accounts[0], gas: "1000000" })
+
 
     // using the instance of Web3  如果参数是一个的箭头函数 x => x^2
     // 这里我们的web3可以和很多的数字货币互动，这里是使用的eth
@@ -24,6 +31,7 @@ beforeEach(async () => {
 describe("Inbox", () => {
     it("deploys a contract", () => {
         console.log(accounts);
+        console.log(inbox);
 
     })
 })
